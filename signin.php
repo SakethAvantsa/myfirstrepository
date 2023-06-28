9e520a35cf5b3c066cafcbbcb2e2d6df422748bf
SakethAvantsa <129094956+SakethAvantsa@users.noreply.github.com>
2023-06-28 16:20:40 +0530

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blood Bank Management System - Sign In</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<h1>Blood Bank Management System</h1>
+</header>
+<main>
+<h2>Sign In</h2>
+<form action="signin.php" method="post">
+<input type="email" name="email" placeholder="Email">
+<input type="password" name="password" placeholder="Password">
+<input type="submit" value="Sign In">
+<a href="forgotpassword.html">Forgot Password?</a>
+</form>
+</main>
+<footer>
+<p>Copyright &copy; 2023 Blood Bank Management System</p>
+</footer>
+</body>
+</html>
