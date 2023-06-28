f254fcc0fed4aed3ae21f674063d7525a3873a71
SakethAvantsa <129094956+SakethAvantsa@users.noreply.github.com>
2023-06-28 16:21:55 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blood Bank Management System - Sign Up</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+<h1>Blood Bank Management System</h1>
+</header>
+<main>
+<h2>Sign Up</h2>
+<form action="signup.php" method="post">
+<input type="text" name="name" placeholder="Name">
+<input type="email" name="email" placeholder="Email">
+<input type="password" name="password" placeholder="Password">
+<input type="password" name="confirm_password" placeholder="Confirm
+Password">
+<input type="submit" value="Sign Up">
+</form>
+</main>
+<footer>
+<p>Copyright &copy; 2023 Blood Bank Management System</p>
+</footer>
+</body>
+</html>
